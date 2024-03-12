c4a69f68aa6b0735cffc4a75c8d8e91aa5ed12eb
BintangDiandra <Bintangdiandraputra@gmail.com>
2024-03-12 10:21:33 +0700

    Belajar-Javascript

--- Function.js
+++ Function.js
@@ -8,12 +8,17 @@
 // }
 
 // alert(addName('Bintang Diandra Putra'))
-function addName(firstname, lastname) {         // pengiriman data ke function
-    console.log('Nama depan : ', firstname)     //function firstname, lastname anggap saja data 1 (first name) data ke 2 (lastname)
-    console.log('Nama Belakang : ', lastname)   // addName <--- Adalah Function
+// function addName(firstname, lastname) {         // pengiriman data ke function
+//     console.log('Nama depan : ', firstname)     //function firstname, lastname anggap saja data 1 (first name) data ke 2 (lastname)
+//     console.log('Nama belakang : ', lastname)   // addName <--- Adalah Function
+// }
+//             // 'Bintang Diandra Putra', 'Diandra Putra' <=---- ini adalah Argumen yang kita ketik
+// addName('Bintang Diandra Putra', 'Diandra Putra')     // <------- ini semua bisa di sebut patching data patching parameter patching value dll (intinya tentang patching)
+
+function addName() {
+    console.log(arguments[0])      //Arguments pemanggilan langsung bisa di tambahkan  ['1', '2', '3'] untuk memanggi data
+    console.log(arguments[1])
+    console.log(arguments[2])
 }
-            // 'Bintang Diandra Putra', 'Diandra Putra' <=---- ini adalah Argumen yang kita ketik
-addName('Bintang Diandra Putra', 'Diandra Putra')     // <------- ini semua bisa di sebut patching data patching parameter patching value dll (intinya tentang patching)
 
-
-alert('Hello World ! Its Me Bintang')
+addName('Bintang Diandra Putra', 'Diandra Putra', 'Aril Noah')

acd9ebe5cc48452a119051a8678354d04d14afe8
BintangDiandra <Bintangdiandraputra@gmail.com>
2024-03-13 17:39:36 +0700

    Belajar-javascript

--- Function.js
+++ Function.js
@@ -28,11 +28,49 @@
 
 
 // Contoh Function dan Looping
-function addName() {
-    for (let i = 0; i < arguments.length; i++) {
-        console.log('Nama Lengkap : ', arguments[i])
-        
-    }
+// function addName() {
+//     for (let i = 0; i < arguments.length; i++) {
+//         console.log('Nama Lengkap : ', arguments[i])    // <-------- Arguments bisa di kostum misal ada 4 data tapi di ambil 1 tinggal ganti i ke argument['sesuai urutan data']
+//                                                         // i [index] mengambil data
+//     }
+// }
+
+// addName('Bintang Diandra Putra', 'Diandra Putra', 'Aril Noah', 'Keroro Ampas',)
+
+// Metode Pertama pembuatan function
+// function addName(firstname, lastname) {
+//     console.log(firstname, lastname)
+// }
+
+// addName('Riko kucing', 'Joko kucing')
+
+// Metode Kedua pembuatan function digabung dengan variabel
+// function addName(firstname, lastname) {
+//     console.log(firstname, lastname)
+// }
+// addName('Bintang diandra', 'Diandra Putra')
+
+// const sum = new Function('x', 'y', 'console.log(x + y)')
+
+// console.log(sum(1, 2))
+
+//---------------------------------------------------------------  // varibel console.log() harus dibawah !!!!
+
+// Function yang diterapkan di Industri
+
+// cara 1 keyword new function
+const sum1 = new Function('x', 'y', 'console.log(x+y)')
+
+//cara 2 - standar function
+function sum2 (x, y) {
+    console.log(x + y)
 }
 
-addName('Bintang Diandra Putra', 'Diandra Putra', 'Aril Noah', 'Keroro Ampas')
+// cara 3 arrow function
+const sum3 = (x, y) => { 
+    console.log(x + y)
+}
+
+sum1(5, 1)
+sum2(1, 2)
+sum3(1, 4)

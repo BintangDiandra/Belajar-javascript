c62cdb566c8726aeca648282d062f52826ae81bb
BintangDiandra <Bintangdiandraputra@gmail.com>
2024-03-16 16:47:42 +0700

    Belajar-javascript

--- Function.js
+++ Function.js
@@ -1,9 +1,8 @@
-
 // Materi Pertama Function JAVASCRIPT
 
 // *Pemanggilan Function*
 
-// function addName(fullname) {            
+// function addName(fullname) {
 //     return fullname      // Function harus mengembalikan nilai | Kalau return tidak ada maka mengembalikan undefined
 // }
 
@@ -23,9 +22,7 @@
 
 // addName('Bintang Diandra Putra', 'Diandra Putra', 'Aril Noah')
 
-
 // alert('check update')
-
 
 // Contoh Function dan Looping
 // function addName() {
@@ -59,20 +56,76 @@
 // Function yang diterapkan di Industri
 
 // cara 1 keyword new function
-const sum1 = new Function('x', 'y', 'console.log(x+y)')
+const sum1 = new Function("x", "y", "console.log(x+y)");
 
 //cara 2 - standar function
-function sum2 (x, y) {
-    console.log(x + y)
+function sum2(x, y) {
+  console.log(x + y);
 }
 
 // cara 3 arrow function
-const sum3 = (x, y) => { 
-    console.log(x + y)
-}
+const sum3 = (x, y) => {
+  console.log(x + y);
+};
 
-sum1(5, 1)
-sum2(1, 2)
-sum3(1, 4)
+sum1(5, 1);
+sum2(1, 2);
+sum3(1, 4);
 
 // Materi lanjutan
+
+function mergeNames(userId, passworidId) {
+  const mergedNames = [];
+
+  for (let i = 0; i < userId.length; i++) {
+    mergedNames.push(`${userId[i]} & ${passworidId[i]}`);
+  }
+
+  return mergedNames;
+}
+
+// Contoh penggunaan fungsi mergeNames()
+const userId = ["Alice", "Bob", "Charlie"];
+const passworidId = ["Dave", "Eve", "Frank"];
+
+const mergedNames = mergeNames(userId, passworidId);
+console.log(mergedNames);
+
+
+function setCookie(name, value, days) {
+    let expires = "";
+    if (days) {
+        let date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+// Contoh penggunaan:
+setCookie('username', 'john_doe', 30); // Menetapkan cookie 'username' dengan nilai 'john_doe' yang berlaku selama 30 hari
+
+function getCookie(name) {
+    let nameEQ = name + "=";
+    let cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+        let cookie = cookies[i];
+        while (cookie.charAt(0) === ' ') {
+            cookie = cookie.substring(1, cookie.length);
+        }
+        if (cookie.indexOf(nameEQ) === 0) {
+            return cookie.substring(nameEQ.length, cookie.length);
+        }
+    }
+    return null;
+}
+
+// Contoh penggunaan:
+let username = getCookie('username'); // Mendapatkan nilai cookie 'username'
+
+function deleteCookie(name) {
+    document.cookie = name + '=; Max-Age=-99999999;';
+}
+
+// Contoh penggunaan:
+deleteCookie('username'); // Menghapus cookie 'username'
